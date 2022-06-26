433bf69442072080ec743865785c6c6fa108f7fb
Ben Stones <benstones99@gmail.com>
2022-06-26 23:12:19 +0100

    more linting + removing extra semicolon

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,12 +1,12 @@
 import './Display.css';
 
 function Display(props) {
-  return (
-    <div className='display-container'>
-      <h1>{props.expression}</h1>
-      <h1>{props.total}</h1>
-    </div>
-  );
+	return (
+		<div className='display-container'>
+			<h1>{props.expression}</h1>
+			<h1>{props.total}</h1>
+		</div>
+	);
 }
 
 export default Display;
