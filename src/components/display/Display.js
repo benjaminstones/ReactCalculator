910adf428aba58fa27038bfd36c707a31fcb1c80
Ben Stones <benstones99@gmail.com>
2022-06-27 23:09:52 +0100

    display adds unicode into string

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,9 +1,15 @@
 import './Display.css';
 
+const formatUnicode = (outputText) => {
+	let formattedText = outputText.replaceAll('*', '\u00d7')
+	formattedText = formattedText.replaceAll('/', '\u00f7')
+	return(formattedText)
+}
+
 function Display(props) {
 	return (
 		<div className='display-container'>
-			<h1>{props.expression}</h1>
+			<h1>{formatUnicode(props.expression)}</h1>
 			<h1>{props.total}</h1>
 		</div>
 	);
