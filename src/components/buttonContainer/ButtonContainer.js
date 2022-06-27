abdca0d1a59bddc8ae861481dd545fc14e780912
Ben Stones <benstones99@gmail.com>
2022-06-27 22:36:58 +0100

    fixed indents in button container for eslint

--- src/components/buttonContainer/ButtonContainer.js
+++ src/components/buttonContainer/ButtonContainer.js
@@ -4,46 +4,46 @@ import './ButtonContainer.css';
 
 function ButtonContainer(props) {
 
-  const handleClick = (buttonName, buttonType) => {
-    props.clickHandler(buttonName, buttonType);
-  };
+	const handleClick = (buttonName, buttonType) => {
+		props.clickHandler(buttonName, buttonType);
+	};
 
-  return (
-    <div className='button-container'>
-      <div className='button-row'>
-        <Button btnName={'AC'} btnType={UTIL} clickHandler={handleClick} />
-        <Button btnName={'('} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={')'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'del'} btnType={UTIL} clickHandler={handleClick} />
-      </div>
-      <div className='button-row'>
-        <Button btnName={'1'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'2'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'3'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'+'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
-      </div>
-      <div className='button-row'>
-        <Button btnName={'4'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'5'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'6'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'-'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
+	return (
+		<div className='button-container'>
+			<div className='button-row'>
+				<Button btnName={'AC'} btnType={UTIL} clickHandler={handleClick} />
+				<Button btnName={'('} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={')'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'del'} btnType={UTIL} clickHandler={handleClick} />
+			</div>
+			<div className='button-row'>
+				<Button btnName={'1'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'2'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'3'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'+'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
+			</div>
+			<div className='button-row'>
+				<Button btnName={'4'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'5'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'6'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'-'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
 
-      </div>
-      <div className='button-row'>
-        <Button btnName={'7'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'8'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'9'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'*'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
-      </div>
-      <div className='button-row'>
-        <Button btnName={'.'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'0'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
-        <Button btnName={'='} btnType={UTIL} clickHandler={handleClick} />
-        <Button btnName={'/'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
-      </div>
+			</div>
+			<div className='button-row'>
+				<Button btnName={'7'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'8'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'9'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'*'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
+			</div>
+			<div className='button-row'>
+				<Button btnName={'.'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'0'} btnType={NON_OPERATOR_CHAR} clickHandler={handleClick} />
+				<Button btnName={'='} btnType={UTIL} clickHandler={handleClick} />
+				<Button btnName={'/'} btnType={OPERATOR_CHAR} clickHandler={handleClick} />
+			</div>
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default ButtonContainer;
