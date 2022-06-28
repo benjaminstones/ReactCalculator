e67ce8cb06c4f84bc09e703d92dc7a069ee9dec8
Ben Stones <benstones99@gmail.com>
2022-06-28 20:55:48 +0100

    Button tags made consistent - no arrays, all objects

--- src/components/buttonContainer/ButtonContainer.js
+++ src/components/buttonContainer/ButtonContainer.js
@@ -5,7 +5,6 @@ import './ButtonContainer.css';
 function ButtonContainer(props) {
 
 	const handleClick = (buttonName, buttonTags) => {
-		console.log(buttonTags)
 		props.clickHandler(buttonName, buttonTags);
 	};
 
@@ -13,32 +12,32 @@ function ButtonContainer(props) {
 		<div className='button-container'>
 			<div className='button-row'>
 				<Button btnName={'AC'} btnTags={{UTIL}} clickHandler={handleClick} />
-				<Button btnName={'('} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={')'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
+				<Button btnName={'('} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={')'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
 				<Button btnName={'del'} btnTags={{UTIL}} clickHandler={handleClick} />
 			</div>
 			<div className='button-row'>
-				<Button btnName={'1'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={'2'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={'3'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
+				<Button btnName={'1'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={'2'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={'3'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
 				<Button btnName={'+'} btnTags={{OPERATOR_CHAR}} clickHandler={handleClick} />
 			</div>
 			<div className='button-row'>
-				<Button btnName={'4'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={'5'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={'6'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
+				<Button btnName={'4'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={'5'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={'6'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
 				<Button btnName={'-'} btnTags={{OPERATOR_CHAR}} clickHandler={handleClick} />
 
 			</div>
 			<div className='button-row'>
-				<Button btnName={'7'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={'8'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={'9'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
+				<Button btnName={'7'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={'8'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={'9'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
 				<Button btnName={'\u00d7'} btnTags={{OPERATOR_CHAR}} clickHandler={handleClick} />
 			</div>
 			<div className='button-row'>
-				<Button btnName={'.'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
-				<Button btnName={'0'} btnTags={[NON_OPERATOR_CHAR]} clickHandler={handleClick} />
+				<Button btnName={'.'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
+				<Button btnName={'0'} btnTags={{NON_OPERATOR_CHAR}} clickHandler={handleClick} />
 				<Button btnName={'='} btnTags={{UTIL}} clickHandler={handleClick} />
 				<Button btnName={'\u00f7'} btnTags={{OPERATOR_CHAR}} clickHandler={handleClick} />
 			</div>
