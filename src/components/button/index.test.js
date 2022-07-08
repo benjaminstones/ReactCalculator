531a92585820d29d78ea75a6ec547b08462b3c1d
Ben Stones <benstones99@gmail.com>
2022-07-08 18:35:53 +0100

    Button snapshot test

---
+++ src/components/button/index.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import '../../setupTests'
+import Button from './index.js';
+import HIGHLIGHTED from '../../constants/index.js';
+
+let wrapped = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }}/> );
+describe('Button', () => {
+	it('should render the Button Component correctly', () => {   
+		expect(wrapped).toMatchSnapshot();
+	});
+});
