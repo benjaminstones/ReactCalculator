489daa0ee0ca80f6e53f39d02a378b69f4304081
Ben Stones <benstones99@gmail.com>
2022-07-09 13:17:44 +0100

    Added test for Display outputs

--- src/components/display/index.test.js
+++ src/components/display/index.test.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Display from './index.js'
 import '../../setupTests'
+import { StyledDisplayText } from './styles/styled.js';
 
 let wrapped = shallow(<Display expression='2+3=' total='5' />);
 describe('Display', () => {
 	it('should render the Display Component correctly', () => {
 		expect(wrapped).toMatchSnapshot();
 	});
+	it ('should render an expression and total in the display', () => {
+		expect(wrapped.find(StyledDisplayText)).toHaveLength(2);
+	})
 });
