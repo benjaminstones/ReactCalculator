bc3b0ff91f277972cb9ad0544b00b13b97f8f511
Ben Stones <benstones99@gmail.com>
2022-07-09 17:09:42 +0100

    Added button tests (flake?)

--- src/components/button/index.test.js
+++ src/components/button/index.test.js
@@ -2,11 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import '../../setupTests'
 import Button from './index.js';
-import HIGHLIGHTED from '../../constants/index.js';
+import  { HIGHLIGHTED, UNHIGHLIGHTED} from '../../constants/index.js';
+import { StyledUnhighlightedButton, StyledHighlightedButton } from './styles/styled';
 
-let wrapped = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }} />);
 describe('Button', () => {
 	it('should render the Button Component correctly', () => {
+		let wrapped = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }} />);
 		expect(wrapped).toMatchSnapshot();
 	});
+	it('should render an unhighlighted button if the unhighlighted tag is given', () => {
+		let wrapped = shallow(<Button btnName={'AC'} btnTags={{ UNHIGHLIGHTED }} />);
+		expect(wrapped.find(StyledUnhighlightedButton)).toHaveLength(1);
+	});
+
+	//FLAKE?
+	// it('should render a highlighted button if the highlighted tag is given', () => {
+	// 	let wrapped = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }} />);
+	// 	expect(wrapped.find(StyledHighlightedButton)).toHaveLength(1);
+	// });
 });
