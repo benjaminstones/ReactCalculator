66f652360bb7d0810be587b1ea0b92fd612aa871
Ben Stones <benstones99@gmail.com>
2022-07-10 23:08:53 +0100

    clean up button tests

--- src/components/button/index.test.js
+++ src/components/button/index.test.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import '../../setupTests'
 import Button from './index.js';
-import  { HIGHLIGHTED, UNHIGHLIGHTED} from '../../constants/index.js';
-import { StyledUnhighlightedButton, StyledHighlightedButton } from './styles/styled';
+import { HIGHLIGHTED, UNHIGHLIGHTED } from '../../constants/index.js';
+import { StyledUnhighlightedButton } from './styles/styled';
 
 describe('Button', () => {
 	it('should render the Button Component correctly', () => {
-		let wrapped = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }} />);
-		expect(wrapped).toMatchSnapshot();
+		let wrapper = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }} />);
+		expect(wrapper).toMatchSnapshot();
 	});
 	it('should render an unhighlighted button if the unhighlighted tag is given', () => {
-		let wrapped = shallow(<Button btnName={'AC'} btnTags={{ UNHIGHLIGHTED }} />);
-		expect(wrapped.find(StyledUnhighlightedButton)).toHaveLength(1);
+		let wrapper = shallow(<Button btnName={'AC'} btnTags={{ UNHIGHLIGHTED }} />);
+		expect(wrapper.find(StyledUnhighlightedButton)).toHaveLength(1);
 	});
 
 	//FLAKE?
 	// it('should render a highlighted button if the highlighted tag is given', () => {
-	// 	let wrapped = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }} />);
-	// 	expect(wrapped.find(StyledHighlightedButton)).toHaveLength(1);
+	// 	let wrapper = shallow(<Button btnName={'AC'} btnTags={{ HIGHLIGHTED }} />);
+	// 	expect(wrapper.find(StyledHighlightedButton)).toHaveLength(1);
 	// });
 });
