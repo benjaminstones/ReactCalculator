1cb6f98e17f0628ef765307b6873e1c3831a8943
Ben Stones <benstones99@gmail.com>
2022-07-10 23:09:52 +0100

    clean up display tests

--- src/components/display/index.test.js
+++ src/components/display/index.test.js
@@ -4,26 +4,26 @@ import Display from './index.js'
 import '../../setupTests'
 import { StyledDisplayText } from './styles/styled.js';
 
-let wrapped = shallow(<Display expression='2+3=' total='5' />);
+let wrapper = shallow(<Display expression='2+3=' total='5' />);
 describe('Display', () => {
 	it('should render the Display Component correctly', () => {
-		expect(wrapped).toMatchSnapshot();
+		expect(wrapper).toMatchSnapshot();
 	});
-	it ('should render an expression and total in the display', () => {
-		const expressionDisplay = wrapped.find('#expression-display');
-		const totalDisplay = wrapped.find('#total-display')
+	it('should render an expression and total in the display', () => {
+		const expressionDisplay = wrapper.find('#expression-display');
+		const totalDisplay = wrapper.find('#total-display')
 
-		expect(wrapped.find(StyledDisplayText)).toHaveLength(2);
+		expect(wrapper.find(StyledDisplayText)).toHaveLength(2);
 		expect(expressionDisplay.exists()).toBe(true);
 		expect(totalDisplay.exists()).toBe(true);
 	})
 	it('should output the expression correctly', () => {
-		const expressionDisplay = wrapped.find('#expression-display');
+		const expressionDisplay = wrapper.find('#expression-display');
 
 		expect(expressionDisplay.text()).toBe('2+3=')
 	});
 	it('should output the total correctly', () => {
-		const totalDisplay = wrapped.find('#total-display')
+		const totalDisplay = wrapper.find('#total-display')
 
 		expect(totalDisplay.text()).toBe('5')
 	});
