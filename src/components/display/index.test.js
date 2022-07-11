add10175bdb4db5a8c11c3257ec6adf6ccbcda16
Ben Stones <benstones99@gmail.com>
2022-07-11 21:07:19 +0100

    clean up

--- src/components/display/index.test.js
+++ src/components/display/index.test.js
@@ -41,9 +41,8 @@ describe('Output unicode formatter', () => {
 		expect(actual).toEqual(expected);
 	})
 
-	const cases = ['1+2', '2-3', '4.5-6.7', '(8.9+1.2-3.4)=']
-
-	test.each(cases) ('should not alter %p as neither * or / are present', (expression) => {
+	const nonUnicodeExpressions = ['1+2', '2-3', '4.5-6.7', '(8.9+1.2-3.4)=']
+	test.each(nonUnicodeExpressions)('should not alter %p as neither * or / are present', (expression) => {
 		const actual = formatOutputWithUnicode(expression);
 		expect(actual).toEqual(expect.not.stringContaining('\u00d7'));
 		expect(actual).toEqual(expect.not.stringContaining('\u00f7'));
