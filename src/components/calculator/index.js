1af2cb9e4631a154f3cef2a889617351eb3a2241
Ben Stones <benstones99@gmail.com>
2022-07-13 23:28:51 +0100

    wip - added calculator snapshot test

--- src/components/calculator/index.js
+++ src/components/calculator/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useState } from 'react';
 import ButtonContainer from '../../components/buttonContainer';
 import Display from '../../components/display/index.js';
