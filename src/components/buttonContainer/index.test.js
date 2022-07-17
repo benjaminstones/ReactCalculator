9e75bf3fb4b80a2ff29ea894fd9f878316a905ef
Ben Stones <benstones99@gmail.com>
2022-07-17 22:34:42 +0100

    Added butotn row test

--- src/components/buttonContainer/index.test.js
+++ src/components/buttonContainer/index.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import '../../setupTests'
 import ButtonContainer from './index.js';
+import { StyledButtonRow } from './styles/styled';
 
 let wrapper = shallow(<ButtonContainer />);
 describe('ButtonContainer', () => {
 	it('should render the ButtonContainer Component correctly', () => {
 		expect(wrapper).toMatchSnapshot();
 	});
+
+	it('should render 3 rows of buttons', () => {
+		expect(wrapper.find(StyledButtonRow)).toHaveLength(5);
+	})
 });
