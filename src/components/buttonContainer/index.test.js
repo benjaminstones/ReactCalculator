2a074a41f833c274f9b97818d4bccac386b2573b
Ben Stones <benstones99@gmail.com>
2022-07-17 22:35:21 +0100

    Added snapshot test for total number of buttons

--- src/components/buttonContainer/index.test.js
+++ src/components/buttonContainer/index.test.js
@@ -3,6 +3,7 @@ import { shallow } from 'enzyme';
 import '../../setupTests'
 import ButtonContainer from './index.js';
 import { StyledButtonRow } from './styles/styled';
+import Button from '../button/index.js'
 
 let wrapper = shallow(<ButtonContainer />);
 describe('ButtonContainer', () => {
@@ -12,5 +13,9 @@ describe('ButtonContainer', () => {
 
 	it('should render 3 rows of buttons', () => {
 		expect(wrapper.find(StyledButtonRow)).toHaveLength(5);
+	})
+
+	it('should render 20 buttons', () => {
+		expect(wrapper.find(Button)).toHaveLength(20);
 	})
 });
