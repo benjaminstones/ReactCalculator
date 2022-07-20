bbe46c93334263984f6d9bbfe95c164ffde172ac
Ben Stones <benstones99@gmail.com>
2022-07-20 22:51:17 +0100

    Added cypress test for the AC functionality

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,5 +10,17 @@ describe('My First Test', () => {
 		cy.get('[data-cy="expression"]').contains('45+6=')
 		cy.get('[data-cy="total"]').contains('51')
 	})
+	it('Clears the contents of the display', () => {
+		cy.visit('http://localhost:3000/ReactCalculator')
+		cy.contains('4').click();
+		cy.contains('+').click();
+		cy.contains('6').click();
+		cy.contains('=').click();
+		cy.get('[data-cy="expression"]').contains('4+6=')
+		cy.get('[data-cy="total"]').contains('10')
+		cy.contains('AC').click();
+		cy.get('[data-cy="expression"]').contains('0')
+		cy.get('[data-cy="total"]').contains('0')
+	})
   
 })
