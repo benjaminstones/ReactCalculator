9eb804df905a90c317871d81a99f75a43fb23c7c
Ben Stones <benstones99@gmail.com>
2022-07-20 22:57:05 +0100

    Added cypress test for subtraction

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,6 +10,16 @@ describe('My First Test', () => {
 		cy.get('[data-cy="expression"]').contains('45+6=')
 		cy.get('[data-cy="total"]').contains('51')
 	})
+	it('Performs a single subtraction', () => {
+		cy.visit('http://localhost:3000/ReactCalculator')
+		cy.contains('4').click();
+		cy.contains('5').click();
+		cy.contains('-').click();
+		cy.contains('6').click();
+		cy.contains('=').click();
+		cy.get('[data-cy="expression"]').contains('45-6=')
+		cy.get('[data-cy="total"]').contains('39')
+	})
 	it('Clears the contents of the display', () => {
 		cy.visit('http://localhost:3000/ReactCalculator')
 		cy.contains('4').click();
